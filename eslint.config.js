afa828bdb140909611ebf8aaf7c36ccce54f77eb
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-11 22:30:35 -0500

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config';
+
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+  },
+  {
+    // Remember to specify the file glob here, otherwise it might cause the vue plugin to handle non-vue files
+    files: ['**/*.vue'],
+    rules: {
+      'vue/operator-linebreak': ['error', 'before'],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: {
+          max: 1,
+        },
+        multiline: {
+          max: 1,
+        },
+      }],
+      'vue/first-attribute-linebreak': ['error', {
+        singleline: 'ignore',
+        multiline: 'below',
+      }],
+    },
+  },
+  {
+    // Without `files`, they are general rules for all files
+    rules: {
+      'style/semi': ['error', 'always'],
+    },
+  },
+);
